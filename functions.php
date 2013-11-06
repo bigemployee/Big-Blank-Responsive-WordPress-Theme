12bd970847fe50982722c9ebeb08f16969b928e8
Norik Davtian <norik@bigemployee.com>
2013-11-05 16:29:40 -0800

    Added automatic feed links to theme support
    Signed-off-by: Norik Davtian <norik@bigemployee.com>

--- functions.php
+++ functions.php
@@ -45,6 +45,9 @@ if (!function_exists('be_setup')) {
 	 */
          add_editor_style( array( 'css/editor-style.css') );
         
+	// Adds RSS feed links to <head> for posts and comments.
+	add_theme_support( 'automatic-feed-links' );
+        
         // Switches default core markup for search form, comment form, and comments
         // to output valid HTML5.
         add_theme_support('html5', array('search-form', 'comment-form', 'comment-list'));
